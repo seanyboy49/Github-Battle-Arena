a10674c5d6bfdee75fee2fbd9301545d87dbd1b9
Sean Lee <seanboramlee@gmail.com>
2016-12-08 14:27:58 -0800

    add styling to confirm battle as well as player info

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,5 +1,8 @@
 const React = require('react');
 const PropTypes = React.PropTypes
+const styles = require('../styles')
+const Link = require('react-router').Link
+
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -8,7 +11,27 @@ function puke (object) {
 const ConfirmBattle = props => {
   return props.isLoading === true
   ? <p> LOADING! </p>
-  : <div> CONFIRM BATTLE! {puke(props)} </div>
+: <div className='jumbotron col-sm-12 text-center' style={styles.transparentBG}>
+    <h1>Confirm Players</h1>
+    <div className='col-sm-8 col-sm-offset-2'>
+      <div className='col-sm-6'>
+        <p className='lead'>Player 1</p>
+        {puke(props.playersInfo[0])}
+      </div>
+      <div className='col-sm-6'>
+        <p className='lead'>Player 2</p>
+        {puke(props.playersInfo[1])}
+      </div>
+    </div>
+    <div className='col-sm-8 col-sm-offset-2'>
+      <div className='col-sm-12' style={styles.space}>
+        <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>INITIATE BATTLE!</button>
+      </div>
+      <div className='col-sm-12' style={styles.space}>
+        LINK TO /PLAYER ONE
+      </div>
+    </div>
+  </div>
 }
 
 ConfirmBattle.propTypes = {

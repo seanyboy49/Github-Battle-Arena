023360ea6d791f83d12b70d6131a151cbeeb8db1
Sean Lee <seanboramlee@gmail.com>
2016-12-12 13:15:22 -0800

    remove buggy transition animations

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,12 +6,7 @@ class Main extends React.Component{
   render() {
     return (
       <div className='main-container'>
-        <ReactCSSTransitionGroup
-          transitionName="appear"
-          transitionEnterTimeout={500}
-          transitionLeaveTimeOut={500}>
-            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
-        </ReactCSSTransitionGroup>
+        {this.props.children}
       </div>
     )
   }

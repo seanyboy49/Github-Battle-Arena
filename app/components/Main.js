09e29e1af6bf629140250e91a89cd5b536c4bbd3
Sean Lee <seanboramlee@gmail.com>
2016-12-11 21:28:47 -0800

    add css transitions between pages

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,17 @@
 const React = require('react');
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group')
+require('../main.css');
 
 class Main extends React.Component{
   render() {
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeOut={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }

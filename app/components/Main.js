b18900dd77e2f180229698332657adee7616759f
Sean Lee <seanboramlee@gmail.com>
2016-12-12 14:01:34 -0800

    fix css animation typo

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,7 +6,12 @@ class Main extends React.Component{
   render() {
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }

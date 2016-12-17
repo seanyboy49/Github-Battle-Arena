13b690293bb1768709f8c202196a16630f876a15
Sean Lee <seanboramlee@gmail.com>
2016-12-16 16:08:59 -0800

    Fix header title

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component{
   render() {
     return (
       <MainContainer>
-        <h1>Github Battle</h1>
+        <h1>GitHub Battle Arena</h1>
         <p className='lead'> What even is React </p>
         <Link to='/playerOne'>
           <button type='button' className='btn btn-lg btn-success'>Get Started</button>
